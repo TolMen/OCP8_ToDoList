4098e65bf994cc6716e4ee7b81d03d846a96e8f1
TolMen <cotton.frachisse@gmail.com>
2024-10-28 12:13:26 +0100

    Optimized code

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,8 +11,6 @@ class DefaultController extends AbstractController
     #[Route('/', name: 'homepage')]
     public function index(): Response
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+        return $this->render('default/index.html.twig');
     }
 }
